5662510e5ce8b01ee060afe2bdcdcfeed0843657
noriakis <31095487+noriakis@users.noreply.github.com>
2024-02-01 11:52:02 +0900

    add sample data

---
+++ R/loadExample.R
@@ -0,0 +1,12 @@
+#' loadExample
+#' load example input data for profileMetagenome
+#' 
+#' @export
+#' @return list of 16S copy number, KO copy number, BLAST result, sequence abundance
+loadExample <- function() {
+    seqtab <- readRDS(system.file("extdata", "example_seqtab.rds", package = "piphillinR"))
+    kocn <- readRDS(system.file("extdata", "example_KOCN.rds", package = "piphillinR"))
+    blast <- readRDS(system.file("extdata", "example_blast.rds", package = "piphillinR"))
+    cn <- readRDS(system.file("extdata", "example_16SCN.rds", package = "piphillinR"))
+    list(cn, kocn, blast, seqtab)
+}
