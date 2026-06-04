a1d0c899904fbd1220005259e88e0fe4dd95d1c1
noriakis <nori@hgc.jp>
2026-06-04 10:24:54 +0900

    maxaccepts and maxrejects option

--- R/alignSequences.R
+++ R/alignSequences.R
@@ -8,9 +8,12 @@
 #' @param out output file of vsearch
 #' @param pctid percent ID, vsearch parameter
 #' @param threads thread number
+#' @param maxaccepts maxaccepts parameter in vsearch
+#' @param maxrejects maxrejects parameter in vsearch
 #' @return output the blast output to the specified path
 #' @export
-alignSequences <- function(repSeqPath, refPath, out, pctid=0.97, threads=1) {
+alignSequences <- function(repSeqPath, refPath, out, pctid=0.97, threads=1,
+    maxaccepts=8, maxrejects=64) {
 	res <- system(paste("vsearch --usearch_global",
     repSeqPath,
     "--db",
@@ -18,8 +21,8 @@ alignSequences <- function(repSeqPath, refPath, out, pctid=0.97, threads=1) {
     "--id",
     pctid,
     '--top_hits_only',
-    '--maxaccepts', '0',
-    '--maxrejects', '0',
+    '--maxaccepts', maxaccepts,
+    '--maxrejects', maxrejects,
     '--uc_allhits',
     '--blast6out',
     out,
